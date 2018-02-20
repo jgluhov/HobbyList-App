57413419d6a970a3683a350da2b168abcf17beb9
Evgeny Glukhov <evglukhov@luxoft.com>
2018-02-20 14:29:03 +0300

    add real hobbies

--- src/main.ts
+++ src/main.ts
@@ -20,28 +20,18 @@ async function main(): Promise<void> {
 
 function initiateStore(): void {
     Store._fill([
-        new Hobby('own-hobby-1'),
-        new Hobby('own-hobby-2'),
-        new Hobby('own-hobby-3'),
-        new Hobby('own-hobby-4'),
-        new Hobby('own-hobby-5'),
-        new Hobby('own-hobby-6'),
-        new Hobby('own-hobby-7'),
-        new Hobby('own-hobby-8'),
-        new Hobby('own-hobby-9'),
-        new Hobby('own-hobby-10'),
-        new Hobby('friend-hobby-1', Belonging.FRIEND),
-        new Hobby('friend-hobby-2', Belonging.FRIEND),
-        new Hobby('friend-hobby-3', Belonging.FRIEND),
-        new Hobby('friend-hobby-4', Belonging.FRIEND),
-        new Hobby('friend-hobby-5', Belonging.FRIEND),
-        new Hobby('friend-hobby-6', Belonging.FRIEND),
-        new Hobby('friend-hobby-7', Belonging.FRIEND),
-        new Hobby('friend-hobby-8', Belonging.FRIEND),
-        new Hobby('friend-hobby-9', Belonging.FRIEND),
-        new Hobby('friend-hobby-10', Belonging.FRIEND),
-        new Hobby('friend-hobby-11', Belonging.FRIEND),
-        new Hobby('friend-hobby-12', Belonging.FRIEND)
+        new Hobby('Дабстеп'),
+        new Hobby('саксофон'),
+        new Hobby('классика'),
+        new Hobby('рок'),
+        new Hobby('боевики'),
+        new Hobby('Сноуборд'),
+        new Hobby('Компьтер'),
+        new Hobby('Typescript'),
+        new Hobby('Любовь'),
+        new Hobby('Мечтать'),
+        new Hobby('Fallout 3 OST', Belonging.FRIEND),
+        new Hobby('Doom metal', Belonging.FRIEND)
     ]);
 }
 
